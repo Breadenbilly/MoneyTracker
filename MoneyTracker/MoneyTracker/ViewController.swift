94eba1dc230c7984b721956e94e144c6e32c014a
Timur <breadenbilly@icloud.com>
2025-06-20 16:40:32 +0300

    Project Created & Initialised

---
+++ MoneyTracker/MoneyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyTracker
+//
+//  Created by Timur Mamedov on 20.6.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
